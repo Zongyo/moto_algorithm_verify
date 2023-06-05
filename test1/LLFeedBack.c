77f5cfc46c3057ae3f6b191ae6dca724de358061
ZongYo <t9014422@gmail.com>
2023-06-06 01:21:19 +0800

    修改飽和參數

--- test1/LLFeedBack.c
+++ test1/LLFeedBack.c
@@ -6,6 +6,7 @@ LLFeedBack.c
 #define TO_COMPILE_LIB_C
 #include "LLFeedBack.h"
 
+
 LLFeedBackStr_t LLFeedBack_str = {
 	.LLLimit = MAX_LEAD_LAG_COUNT,
 	.LLLimit_Expon = MAX_LEAD_LAG_EXPON,
@@ -17,16 +18,25 @@ LLFeedBackStr_t LLFeedBack_str = {
 void LLFeedBack_lay(LLFeedBackStr_t* Str_p) {
 	Str_p->CountOut_p = &Str_p->CountOut;
 	Str_p->DiffCountAcc_p = &Str_p->DiffCountAcc;
+	Str_p->DiffCountAcc = 0;
 }
 
 uint8_t LLFeedBack_step(void* void_p) {
 	LLFeedBackStr_t* Str_p = (LLFeedBackStr_t*)void_p;
 	int16_t LeadLag;
-	//F16.0=F0.16*F16.0+F8.0=(F16*F16)>>16 +F8.0
 	Str_p->DiffCountAcc =(((int32_t)Str_p->Lambda * (int32_t)Str_p->DiffCountAcc) >> Str_p->Lambda_Expon ) + *Str_p->DiffCountIn_p;
+	
+	if (Str_p->DiffCountAcc > SATURATE)
+	{
+		Str_p->DiffCountAcc = SATURATE;
+	}
+	else if (Str_p->DiffCountAcc < -SATURATE) {
+		Str_p->DiffCountAcc = -SATURATE;
+	}
+
 	//為何有負號
 	//diff 為int8 , int8 +-2.81度
-	Str_p->DiffCountAcc = Str_p->DiffCountAcc * 5;
+	//Str_p->DiffCountAcc = 0.95*Str_p->DiffCountAcc + *Str_p->DiffCountIn_p;
 	LeadLag = ((int32_t)Str_p->LLKi * Str_p->DiffCountAcc) >> Str_p->LLKi_Expon;//F7.9 <= F0.9*F16.0=(F8.0*F16.0)>>9=F15.9
 	if (LeadLag > Str_p->LLLimit)
 		LeadLag = Str_p->LLLimit;
